7d9f0c40c5eaf7fc8e3ea5ac94fe3ec4771bd532
Barış Şener <baris@dealforward.com>
2024-10-09 22:37:49 +0300

    components are added

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Button } from "@/components/ui/button";
 
 const index = () => {
   return (
-    <div>index</div>
-  )
-}
+    <div>
+      index
+      <Button>Click me</Button>
+    </div>
+  );
+};
 
-export default index
+export default index;
